92a0bad4ea56cefa2c1b36cd05f0f0db2167f6a6
BurningFalls <spdlqj7791@naver.com>
2020-08-04 16:11:32 +0900

    Success: 1927

---
+++ Baekjoon_All/(1927) 최소 힙.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+	int N;
+	int x;
+	priority_queue<int, vector<int>, greater<int>> pq;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> x;
+		if (x == 0) {
+			if (pq.empty())
+				cout << "0\n";
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(x);
+		}
+	}
+
+	return 0;
+}
