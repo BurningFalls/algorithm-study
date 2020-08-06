52ae2e88c737debe1b0e0bcbf0ddcd8b965cd9cc
BurningFalls <spdlqj7791@naver.com>
2020-08-07 00:37:41 +0900

    Success: 14681

---
+++ Baekjoon_All/(14681) 사분면 고르기.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a;
+	cin >> b;
+	if (a > 0 && b > 0) {
+		cout << "1" << endl;
+	}
+	else if (a < 0 && b > 0) {
+		cout << "2" << endl;
+	}
+	else if (a > 0 && b < 0) {
+		cout << "4" << endl;
+	}
+	else if (a < 0 && b < 0) {
+		cout << "3" << endl;
+	}
+	return 0;
+}
