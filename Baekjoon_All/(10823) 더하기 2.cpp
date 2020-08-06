85d12fb9ff14501ba449ae6240795fa4e9e279a0
BurningFalls <spdlqj7791@naver.com>
2020-08-07 00:21:31 +0900

    Success: 10823

---
+++ Baekjoon_All/(10823) 더하기 2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+int main() {
+	string row_str;
+	string str = "";
+	int sum = 0;
+	int temp;
+	string sub_str;
+	int start_node = 0;
+	while (getline(cin, row_str)) {
+		str += row_str;
+	}
+	for (int i = 0; i < str.size(); i++) {
+		if (str[i] == ',') {
+			sub_str = str.substr(start_node, i - start_node);
+			temp = 0;
+			for (int j = 0; j < sub_str.size(); j++) {
+				temp += ((sub_str[j] - '0') * pow(10, sub_str.size() - j - 1));
+			}
+			sum += temp;
+			start_node = i + 1;
+		}
+	}
+	sub_str = str.substr(start_node, str.size() - start_node);
+	temp = 0;
+	for (int j = 0; j < sub_str.size(); j++) {
+		temp += ((sub_str[j] - '0') * pow(10, sub_str.size() - j - 1));
+	}
+	sum += temp;
+	cout << sum;
+
+	return 0;
+}
