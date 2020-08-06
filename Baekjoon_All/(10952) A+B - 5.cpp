35f261329973a17f9c31deaac0e913751d634b09
BurningFalls <spdlqj7791@naver.com>
2020-08-07 00:22:57 +0900

    Success: 10952

---
+++ Baekjoon_All/(10952) A+B - 5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	while (true) {
+		cin >> a >> b;
+		if (a == 0 && b == 0) {
+			break;
+		}
+		cout << a + b << endl;
+	}
+
+	return 0;
+}
