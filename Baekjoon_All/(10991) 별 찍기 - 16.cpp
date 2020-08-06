d07a65b6c92fb977e836ae87aec3a989d394f204
BurningFalls <spdlqj7791@naver.com>
2020-08-07 00:23:58 +0900

    Success: 10991

---
+++ Baekjoon_All/(10991) 별 찍기 - 16.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N - 1 - i; j++) {
+			cout << " ";
+		}
+		for (int j = 0; j < i + 1; j++) {
+			cout << "* ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
