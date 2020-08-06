43298def92f62ef3dca0dc08507e7028c2ee8555
BurningFalls <spdlqj7791@naver.com>
2020-08-07 00:44:22 +0900

    Success: 16394

---
+++ Baekjoon_All/(16394) 홍익대학교.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N;
+	cin >> N;
+	cout << N - 1946;
+
+	return 0;
+}
