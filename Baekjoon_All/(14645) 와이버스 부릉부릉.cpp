af1d740cd9ae035ace94c690621570061bb7f3e2
BurningFalls <spdlqj7791@naver.com>
2020-08-07 00:37:30 +0900

    Success: 14645

---
+++ Baekjoon_All/(14645) 와이버스 부릉부릉.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, K;
+	int A, B;
+	cin >> N >> K;
+	for (int i = 0; i < N; i++) {
+		cin >> A >> B;
+	}
+	cout << "ºñ¿ÍÀÌ";
+
+	return 0;
+}
