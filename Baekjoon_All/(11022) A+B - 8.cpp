f87b2d32dfcb655696f96f60d4643dee787853f8
BurningFalls <spdlqj7791@naver.com>
2020-08-07 00:25:21 +0900

    Success: 11022

---
+++ Baekjoon_All/(11022) A+B - 8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+
+	int T, a, b;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> a >> b;
+		cout << "Case #" << i + 1 << ": " << a << " + " << b << " = " << a + b << endl;
+	}
+
+	return 0;
+}
