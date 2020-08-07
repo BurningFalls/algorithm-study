9071f8bac538ba128783226c2cc736b62b3f8322
BurningFalls <spdlqj7791@naver.com>
2020-08-07 16:36:39 +0900

    Success: 9316

---
+++ Baekjoon_All/(9316) Hello Judge.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cout << "Hello World, Judge " << i << "!" << "\n";
+	}
+
+	return 0;
+}
