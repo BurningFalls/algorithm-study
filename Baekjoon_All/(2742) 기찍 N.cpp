23e0ccb7b6effc597a7566b3b3e609d97bf962ae
BurningFalls <spdlqj7791@naver.com>
2020-08-07 16:14:47 +0900

    Success: 2742

---
+++ Baekjoon_All/(2742) 기찍 N.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+	int n;
+	cin >> n;
+	for (int i = n; i >= 1; i--) {
+		cout << i << "\n";
+	}
+	return 0;
+}
