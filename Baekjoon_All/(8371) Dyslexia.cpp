0bf12a474823920a2f339b4c6815a0f391563401
BurningFalls <spdlqj7791@naver.com>
2020-08-07 16:34:47 +0900

    Success: 8371

---
+++ Baekjoon_All/(8371) Dyslexia.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int N;
+	string a, b;
+	int cnt = 0;
+	cin >> N;
+	cin >> a;
+	cin >> b;
+	for (int i = 0; i < N; i++) {
+		if (a[i] != b[i])
+			cnt++;
+	}
+	cout << cnt;
+
+	return 0;
+}
