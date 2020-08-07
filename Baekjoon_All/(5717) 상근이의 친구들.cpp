143848cdf178df92ee32c66e650949f50d22c5f6
BurningFalls <spdlqj7791@naver.com>
2020-08-07 16:33:06 +0900

    Success: 5717

---
+++ Baekjoon_All/(5717) 상근이의 친구들.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int M, F;
+	while (true) {
+		cin >> M >> F;
+		if (M == 0 && F == 0)
+			break;
+		cout << M + F << "\n";
+	}
+
+	return 0;
+}
