b6338978672e44ed2929cdb25aa131ae0df8751a
BurningFalls <spdlqj7791@naver.com>
2020-08-07 16:08:05 +0900

    Success: 2445

---
+++ Baekjoon_All/(2445) 별 찍기 - 8.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 0; i < N - 1; i++) {
+		for (int j = 0; j < i + 1; j++) {
+			cout << "*";
+		}
+		for (int j = 0; j < 2 * N - 2 - 2 * i; j++) {
+			cout << " ";
+		}
+		for (int j = 0; j < i + 1; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < 2 * N; i++) {
+		cout << "*";
+	}
+	cout << endl;
+	for (int i = 0; i < N - 1; i++) {
+		for (int j = 0; j < N - 1 - i; j++) {
+			cout << "*";
+		}
+		for (int j = 0; j < 2 * i + 2; j++) {
+			cout << " ";
+		}
+		for (int j = 0; j < N - 1 - i; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
