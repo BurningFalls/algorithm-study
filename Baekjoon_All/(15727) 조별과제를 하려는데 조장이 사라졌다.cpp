b2bd84dbdadb2329e6c795b650abe39ad7f33b26
BurningFalls <spdlqj7791@naver.com>
2020-08-17 02:51:27 +0900

    Success: 15727

---
+++ Baekjoon_All/(15727) 조별과제를 하려는데 조장이 사라졌다.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+
+int main() {
+	FASTIO;
+	int L;
+	cin >> L;
+	cout << (L - 1) / 5 + 1;
+
+	return 0;
+}
