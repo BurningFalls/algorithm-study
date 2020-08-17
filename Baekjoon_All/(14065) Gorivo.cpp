6c173a942440b29eac71674db08f93a32a82d9d4
BurningFalls <spdlqj7791@naver.com>
2020-08-17 22:33:22 +0900

    Success: 14065

---
+++ Baekjoon_All/(14065) Gorivo.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+
+int main() {
+	double x;
+	cin >> x;
+	cout << fixed << setprecision(6);
+	cout << 378541178.4 / (1609344 * x);
+
+	return 0;
+}
