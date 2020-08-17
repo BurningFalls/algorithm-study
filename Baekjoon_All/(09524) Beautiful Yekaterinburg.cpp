33c365b85a441e7bc9e5b8984b2fd26792d8b599
BurningFalls <spdlqj7791@naver.com>
2020-08-17 22:32:44 +0900

    Success: 09524

---
+++ Baekjoon_All/(09524) Beautiful Yekaterinburg.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+
+int main() {
+	int D;
+	int arr[5] = { 0, 1, 7, 2, 3 };
+	cin >> D;
+	cout << arr[D];
+
+	return 0;
+}
