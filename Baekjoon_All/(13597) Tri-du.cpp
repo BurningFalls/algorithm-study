6e51d5ec90d996bff74e16fefcc0bf728061c68c
BurningFalls <spdlqj7791@naver.com>
2020-08-17 22:33:04 +0900

    Success: 13597

---
+++ Baekjoon_All/(13597) Tri-du.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	if (a == b)
+		cout << a;
+	else
+		cout << max(a, b);
+
+	return 0;
+}
