d30f0e1d7290eff68ba962b46290fd869d85f847
BurningFalls <spdlqj7791@naver.com>
2020-08-19 01:27:24 +0900

    Success: 15726

---
+++ Baekjoon_All/(15726) 이칙연산.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+
+int main() {
+	double A, B, C;
+	cin >> A >> B >> C;
+	cout << max(int(A * B / C), int(A / B * C));
+
+	return 0;
+}
