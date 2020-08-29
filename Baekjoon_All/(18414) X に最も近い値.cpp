df3a9e65706b31e504ddd158f8a4c290621bd513
BurningFalls <spdlqj7791@naver.com>
2020-08-29 18:26:17 +0900

    Success: 18414

---
+++ Baekjoon_All/(18414) X に最も近い値.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+
+int main() {
+	FASTIO;
+	int X, L, R;
+	cin >> X >> L >> R;
+	if (X <= L)
+		cout << L;
+	else if (R <= X)
+		cout << R;
+	else
+		cout << X;
+
+	return 0;
+}
