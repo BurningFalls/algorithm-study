e13c1694dab890f384add5e5d58db5f945ed889c
BurningFalls <spdlqj7791@naver.com>
2020-08-29 18:25:54 +0900

    Success: 17874

---
+++ Baekjoon_All/(17874) Piece of Cake!.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+
+int main() {
+	FASTIO;
+	int n, h, v;
+	cin >> n >> h >> v;
+	cout << 4 * max(h, n - h) * max(v, n - v);
+
+	return 0;
+}
