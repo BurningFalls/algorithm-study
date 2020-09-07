575efaf8edbdc07b0a2d582e2f8797a0c52d82bd
BurningFalls <spdlqj7791@naver.com>
2020-09-07 23:07:01 +0900

    Success: 03507

---
+++ Baekjoon_All/(03507) Automated Telephone Exchange.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+
+int main() {
+	FASTIO;
+	int n;
+	cin >> n;
+	if (n >= 100 && n <= 198)
+		cout << 199 - n;
+	else
+		cout << 0;
+
+	return 0;
+}
