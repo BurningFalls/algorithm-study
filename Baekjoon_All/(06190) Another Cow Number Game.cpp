e16d7ed2a9dfcb2241858bafa4a8e7317a8b9183
BurningFalls <spdlqj7791@naver.com>
2020-09-10 23:02:44 +0900

    Success: 06190

---
+++ Baekjoon_All/(06190) Another Cow Number Game.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+
+int main() {
+	FASTIO;
+	int N;
+	int score = 0;
+	cin >> N;
+	while (N != 1) {
+		if (N % 2 == 1)
+			N = N * 3 + 1;
+		else if (N % 2 == 0)
+			N /= 2;
+		score++;
+	}
+	cout << score;
+
+	return 0;
+}
