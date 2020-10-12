11ff862eab4ccbd3162fed415c7afad0a82c58b5
BurningFalls <spdlqj7791@naver.com>
2020-10-13 01:31:59 +0900

    Success: 10406

---
+++ Baekjoon_All/(10000~10999)/(10406) The fellowship of the ring.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	int a, b, c;
+	int num;
+	int cnt = 0;
+	cin >> a >> b >> c;
+	FOR(i, 1, c) {
+		cin >> num;
+		if (a <= num && num <= b)
+			cnt++;
+	}
+	cout << cnt;
+
+	return 0;
+}
