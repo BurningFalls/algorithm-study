73bbdfe79b97cfdfa679737be429d598ea653655
BurningFalls <spdlqj7791@naver.com>
2021-01-02 18:51:23 +0900

    Success: 01867

---
+++ Baekjoon_All/(01000~01999)/(01867) 돌멩이 제거 [Bitpartite Matching].cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+#define MAX 500 + 1
+
+int N, K;
+vector<int> wanted[MAX];
+int B_match[MAX];
+bool visited[MAX];
+
+bool DFS(int a) {
+	int len = wanted[a].size();
+	FOR(i, 0, len - 1) {
+		int b = wanted[a][i];
+		if (visited[b]) continue;
+		visited[b] = 1;
+		if (B_match[b] == -1 || DFS(B_match[b])) {
+			B_match[b] = a;
+			return true;
+		}
+	}
+	return false;
+}
+
+int BM() {
+	int ans = 0;
+	FOR(i, 1, N) {
+		memset(visited, 0, sizeof(visited));
+		if (DFS(i))
+			ans++;
+	}
+	return ans;
+}
+
+int main() {
+	FASTIO;
+	memset(B_match, -1, sizeof(B_match));
+	cin >> N >> K;
+	FOR(i, 1, K) {
+		int a, b;
+		cin >> a >> b;
+		wanted[a].push_back(b);
+	}
+	cout << BM();
+
+	return 0;
+}
