d7ee39d58d1cb324f4ed4e63aea8601e8f18362e
BurningFalls <spdlqj7791@naver.com>
2021-03-04 10:56:27 +0900

    Success: 14607

---
+++ Baekjoon_All/(14000~14999)/(14607) 피자 (Large).cpp
@@ -0,0 +1,18 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define FOR(i, a, b) for(int i=a;i<=b;i++)
+#define ROF(i, a, b) for(int i=a;i>=b;i--)
+#define pii pair<int, int>
+#define ll long long int
+
+int main() {
+	FASTIO;
+	ll ans, N;
+	cin >> N;
+	ans = (N - 1) * N / 2;
+	cout << ans;
+
+	return 0;
+}
