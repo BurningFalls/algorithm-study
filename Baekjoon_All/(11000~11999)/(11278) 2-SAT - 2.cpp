45cdb4e0bedb3624a6c254821b885b6d348e6ab9
BurningFalls <spdlqj7791@naver.com>
2021-03-16 12:36:21 +0900

    Success: 11278

---
+++ Baekjoon_All/(11000~11999)/(11278) 2-SAT - 2.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define FOR(i, a, b) for(int i=a;i<=b;i++)
+#define ROF(i, a, b) for(int i=a;i>=b;i--)
+#define pii pair<int, int>
+#define ll long long int
+
+int N, M;
+pii group[101];
+bool arr[21] = { 0, };
+bool ans = false;
+bool ans_arr[21] = { 0, };
+
+bool Calc() {
+	FOR(i, 1, M) {
+		int a = group[i].first;
+		int b = group[i].second;
+		bool tmp = 0;
+		if (a < 0 && b < 0) {
+			tmp = (!arr[-a]) | (!arr[-b]);
+		}
+		else if (a < 0 && b > 0) {
+			tmp = (!arr[-a]) | (arr[b]);
+		}
+		else if (a > 0 && b < 0) {
+			tmp = (arr[a]) | (!arr[-b]);
+		}
+		else if (a > 0 && b > 0) {
+			tmp = (arr[a]) | (arr[b]);
+		}
+		if (tmp == 0)
+			return false;
+	}
+	return true;
+}
+
+void DFS(int cnt) {
+	if (cnt > N) {
+		bool result = Calc();
+		if (result) {
+			FOR(i, 1, N)
+				ans_arr[i] = arr[i];
+		}
+		ans = ans | result;
+		return;
+	}
+	FOR(i, 0, 1) {
+		arr[cnt] = i;
+		DFS(cnt + 1);
+	}
+}
+
+int main() {
+	FASTIO;
+	cin >> N >> M;
+	FOR(i, 1, M) {
+		cin >> group[i].first >> group[i].second;
+	}
+	DFS(1);
+	cout << ans << "\n";
+	if (ans) {
+		FOR(i, 1, N)
+			cout << ans_arr[i] << " ";
+	}
+
+	return 0;
+}
