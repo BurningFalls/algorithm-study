30918f54a861a3691aa0f6fa686c2e1518defcda
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-05-10 12:25:52 +0900

    Success: 12021

---
+++ Baekjoon_All/(12000~12999)/(12021) 보물 찾기.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	long double A, B;
+	cin >> A >> B;
+	cout << fixed << setprecision(6);
+	long double ans = sqrt(A * B);
+	cout << ans << " " << ans;
+
+	return 0;
+}
