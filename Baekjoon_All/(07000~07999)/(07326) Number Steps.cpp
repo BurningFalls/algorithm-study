0a0e65503b266f6ba561408b7f071c7c4e459bfb
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-05-31 11:24:51 +0900

    Success: 07326

---
+++ Baekjoon_All/(07000~07999)/(07326) Number Steps.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	FOR(i, 1, N) {
+		int a, b;
+		cin >> a >> b;
+		if (b == a || b + 2 == a) {
+			b = (b / 2) * 2;
+			cout << a + b;
+		}
+		else
+			cout << "No Number";
+		cout << "\n";
+	}
+
+
+	return 0;
+}
