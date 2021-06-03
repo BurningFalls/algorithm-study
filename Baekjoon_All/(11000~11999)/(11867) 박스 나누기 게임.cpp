eb900383a43c3454e8682095ecc710e393a7d235
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-06-03 11:16:08 +0900

    Success: 11867

---
+++ Baekjoon_All/(11000~11999)/(11867) 박스 나누기 게임.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	int N, M;
+	cin >> N >> M;
+	if (N % 2 == 1 && M % 2 == 1)
+		cout << "B";
+	else
+		cout << "A";
+
+
+	return 0;
+}
