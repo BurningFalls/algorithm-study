21d84fc504bbc2f480ae78205608b9005b3c6758
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-06-09 15:12:50 +0900

    Success: 20254

---
+++ Baekjoon_All/(20000~20999)/(20254) Site Score.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << 56 * a + 24 * b + 14 * c + 6 * d;
+
+	return 0;
+}
