c6a1183f0dd2ccabb4acc35e49655d76fcffc18c
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-06-09 17:26:30 +0900

    Success: 20673

---
+++ Baekjoon_All/(20000~20999)/(20673) Covid-19.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	int a, b;
+	cin >> a >> b;
+	if (b <= 10)
+		cout << "White";
+	else if (b >= 30)
+		cout << "Red";
+	else
+		cout << "Yellow";
+
+
+	return 0;
+}
