e1eeb65ced49d9f76c762710b638ab6c7d6cb3e3
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-06-09 16:01:07 +0900

    Success: 19698

---
+++ Baekjoon_All/(19000~19999)/(19698) 헛간 청약.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	int N, W, H, L;
+	cin >> N >> W >> H >> L;
+	int ans = (W / L) * (H / L);
+	cout << min(ans, N);
+
+	return 0;
+}
