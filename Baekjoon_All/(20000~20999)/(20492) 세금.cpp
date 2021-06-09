12be41975529d572ee7f558bb38b15553fde4e63
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-06-09 15:12:55 +0900

    Success: 20492

---
+++ Baekjoon_All/(20000~20999)/(20492) 세금.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	cout << N - N / 100 * 22 << " " << N - N / 100 * 20 / 100 * 22;
+
+
+	return 0;
+}
