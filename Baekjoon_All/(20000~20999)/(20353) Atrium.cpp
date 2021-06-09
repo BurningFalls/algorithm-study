a6e96076256d86ebefe101004b2709d03d6ed07f
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-06-09 17:26:20 +0900

    Success: 20353

---
+++ Baekjoon_All/(20000~20999)/(20353) Atrium.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	long double N;
+	cin >> N;
+	cout << fixed << setprecision(6);
+	cout << 4 * sqrt(N);
+
+	return 0;
+}
