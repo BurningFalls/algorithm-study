e665414e17fc845e9269dfd8c48deb93a5773e32
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-06-10 15:19:09 +0900

    Success: 21591

---
+++ Baekjoon_All/(21000~21999)/(21591) Laptop Sticker.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	a -= 2; b -= 2;
+	bool flag = ((a >= c) && (b >= d));
+	cout << flag;
+
+
+	return 0;
+}
