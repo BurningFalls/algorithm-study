a538bbd453a72f3139b1fcd4a98368fbc30d5032
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-06-10 15:18:40 +0900

    Success: 20976

---
+++ Baekjoon_All/(20000~20999)/(20976) 2 番目に大きい整数.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	int arr[3];
+	FOR(i, 0, 2)
+		cin >> arr[i];
+	sort(arr, arr + 3);
+	cout << arr[1];
+
+
+	return 0;
+}
