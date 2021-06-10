8beb4fbce4b26c369208400a6cbda8d18af78602
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-06-10 15:19:15 +0900

    Success: 21598

---
+++ Baekjoon_All/(21000~21999)/(21598) SciComLove.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	FOR(i, 1, N) {
+		cout << "SciComLove" << "\n";
+	}
+
+
+	return 0;
+}
