c5a020935fbd4ec657b9e6571accce92d7aa0632
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-06-17 16:17:16 +0900

    Success: 21964

---
+++ Baekjoon_All/(21000~21999)/(21964) 선린인터넷고등학교 교가.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	int N;
+	string S;
+	cin >> N >> S;
+	FOR(i, N - 5, N - 1)
+		cout << S[i];
+
+	return 0;
+}
