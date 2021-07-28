67c77dc451c8a012dd2c40585110a796a2730034
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-07-28 17:10:18 +0900

    Success: 21756

---
+++ Baekjoon_All/(21000~21999)/(21756) 지우개.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	int cnt = 0;
+	while (N >= 2) {
+		N /= 2;
+		cnt++;
+	}
+	cout << pow(2, cnt);
+
+	return 0;
+}
