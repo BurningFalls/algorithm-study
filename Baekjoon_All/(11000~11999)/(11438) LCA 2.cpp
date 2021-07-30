f7ea6c500d55e3e9a770ce260dc393a64e5017fe
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-07-30 14:19:18 +0900

    Success: 11438

--- Baekjoon_All/(11000~11999)/(11438) LCA 2.cpp
+++ Baekjoon_All/(11000~11999)/(11438) LCA 2.cpp
@@ -5,67 +5,68 @@ typedef long long int ll;
 #define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
 #define FOR(i,a,b) for(int i=(a);i<=(b);i++)
 #define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
 
-int N, M;
-vector<int> v[100001];
-int Parent[100001][19];
-int d[100001] = { 0, };
+int N;
+vector<int> edge[100001];
+int depth[100001];
+int Parent[100001][20];
+int max_level;
 
-void DFS(int n, int depth) {
-	d[n] = depth;
-	int len = v[n].size();
+void Find_Parent(int cur, int prev) {
+	depth[cur] = depth[prev] + 1;
+	Parent[cur][0] = prev;
+	FOR(i, 1, max_level) {
+		int tmp = Parent[cur][i - 1];
+		Parent[cur][i] = Parent[tmp][i - 1];
+	}
+	int len = edge[cur].size();
 	FOR(i, 0, len - 1) {
-		int nn = v[n][i];
-		if (d[nn]) continue;
-		Parent[nn][0] = n;
-		DFS(nn, depth + 1);
+		int next = edge[cur][i];
+		if (next == prev) continue;
+		Find_Parent(next, cur);
 	}
-}
-
-void Make_Parent() {
-	FOR(j, 1, 18) {
-		FOR(i, 1, N) {
-			Parent[i][j] = Parent[Parent[i][j - 1]][j - 1];
-		}
-	}
-}
-
-int Find(int n1, int n2) {
-	if (d[n2] < d[n1])
-		swap(n2, n1);
-	ROF(i, 18, 0) {
-		int dif = d[n2] - d[n1];
-		if (dif >= (1 << i))
-			n2 = Parent[n2][i];
-	}
-	if (n2 == n1)
-		return n2;
-	ROF(i, 18, 0) {
-		if (Parent[n2][i] != Parent[n1][i]) {
-			n2 = Parent[n2][i];
-			n1 = Parent[n1][i];
-		}
-	}
-	return Parent[n2][0];
 }
 
 int main() {
 	FASTIO;
 	cin >> N;
 	FOR(i, 1, N - 1) {
-		int x, y;
-		cin >> x >> y;
-		v[x].push_back(y);
-		v[y].push_back(x);
+		int a, b;
+		cin >> a >> b;
+		edge[a].push_back(b);
+		edge[b].push_back(a);
 	}
-	DFS(1, 1);
-	Make_Parent();
+	depth[0] = -1;
+	max_level = (int)floor(log2(100001));
+	Find_Parent(1, 0);
+
+	int M;
 	cin >> M;
 	FOR(i, 1, M) {
-		int x, y;
-		cin >> x >> y;
-		cout << Find(x, y) << "\n";
+		int a, b;
+		cin >> a >> b;
+		if (depth[a] != depth[b]) {
+			if (depth[a] > depth[b])
+				swap(a, b);
+			ROF(i, max_level, 0) {
+				if (depth[a] <= depth[Parent[b][i]])
+					b = Parent[b][i];
+			}
+		}
+		int lca = a;
+		if (a != b) {
+			ROF(i, max_level, 0) {
+				if (Parent[a][i] != Parent[b][i]) {
+					a = Parent[a][i];
+					b = Parent[b][i];
+				}
+				lca = Parent[a][i];
+			}
+		}
+		cout << lca << "\n";
 	}
+
 
 	return 0;
 }
