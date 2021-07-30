776445582d8e4e6d6a3eb4cba7bc8b99cefa9e56
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-07-30 17:24:42 +0900

    Success: 05719

--- Baekjoon_All/(05000~05999)/(05719) 거의 최단 경로.cpp
+++ Baekjoon_All/(05000~05999)/(05719) 거의 최단 경로.cpp
@@ -5,60 +5,64 @@ typedef long long int ll;
 #define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
 #define FOR(i,a,b) for(int i=(a);i<=(b);i++)
 #define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
 #define INF 987654321
 
 int N, M;
 int S, D;
-vector<pair<int, int>> vec[501];
-vector<pair<int, int>> trace[501];
+vector<pii> edge[501];
 int dist[501];
+vector<int> trace[501];
+bool visited[501][501];
 
 void DT() {
-	priority_queue<pair<int, int>> pq;
-	FOR(i, 0, N - 1)
+	FOR(i, 0, N - 1) {
 		dist[i] = INF;
+		trace[i].clear();
+	}
+	priority_queue<pii> pq;
 	pq.push({ 0, S });
 	dist[S] = 0;
 	while (!pq.empty()) {
-		int cost = -pq.top().first;
-		int cur = pq.top().second;
+		int cur_cost = -pq.top().first;
+		int cur_node = pq.top().second;
 		pq.pop();
-		if (dist[cur] < cost)
+		if (dist[cur_node] < cur_cost)
 			continue;
-		int len = vec[cur].size();
+		int len = edge[cur_node].size();
 		FOR(i, 0, len - 1) {
-			if (vec[cur][i].second == -1)
+			if (edge[cur_node][i].second == -1)
 				continue;
-			int next = vec[cur][i].first;
-			int next_d = cost + vec[cur][i].second;
-			if (dist[next] > next_d) {
-				dist[next] = next_d;
-				pq.push({ -next_d, next });
-				trace[next].clear();
-				trace[next].push_back({ cur, next_d });
+			int next_node = edge[cur_node][i].first;
+			int next_cost = edge[cur_node][i].second + cur_cost;
+			if (dist[next_node] > next_cost) {
+				dist[next_node] = next_cost;
+				pq.push({ -dist[next_node], next_node });
+				trace[next_node].clear();
+				trace[next_node].push_back(cur_node);
 			}
-			else if (dist[next] == next_d)
-				trace[next].push_back({ cur, next_d });
+			else if (dist[next_node] == next_cost) {
+				trace[next_node].push_back(cur_node);
+			}
 		}
 	}
 }
 
-void BFS() {
-	queue<int> q;
-	q.push(D);
-	while (!q.empty()) {
-		int cur = q.front();
-		q.pop();
-		int len = trace[cur].size();
-		FOR(i, 0, len - 1) {
-			int next = trace[cur][i].first;
-			int len2 = vec[next].size();
-			FOR(j, 0, len2 - 1) {
-				if (vec[next][j].first == cur)
-					vec[next][j].second = -1;
-			}
-			q.push(next);
+void Erase_Edge(int cur) {
+	int len = trace[cur].size();
+	FOR(i, 0, len - 1) {
+		int next = trace[cur][i];
+		if (visited[cur][next]) continue;
+		if (visited[next][cur]) continue;
+		visited[next][cur] = 1;
+		visited[cur][next] = 1;
+		int edge_len = edge[next].size();
+		FOR(j, 0, edge_len - 1) {
+			int tmp = edge[next][j].first;
+			if (tmp == cur)
+				edge[next][j].second = -1;
 		}
+		Erase_Edge(next);
 	}
 }
 
@@ -68,18 +72,17 @@ int main() {
 		cin >> N >> M;
 		if (N == 0 && M == 0)
 			break;
-		FOR(i, 0, N - 1) {
-			trace[i].clear();
-			vec[i].clear();
-		}
 		cin >> S >> D;
-		FOR(i, 0, M - 1) {
-			int u, v, p;
-			cin >> u >> v >> p;
-			vec[u].push_back({ v, p });
+		memset(visited, 0, sizeof(visited));
+		FOR(i, 0, N - 1)
+			edge[i].clear();
+		FOR(i, 1, M) {
+			int U, V, P;
+			cin >> U >> V >> P;
+			edge[U].push_back({ V, P });
 		}
 		DT();
-		BFS();
+		Erase_Edge(D);
 		DT();
 		if (dist[D] == INF)
 			cout << -1;
@@ -87,6 +90,7 @@ int main() {
 			cout << dist[D];
 		cout << "\n";
 	}
+
 
 	return 0;
 }
