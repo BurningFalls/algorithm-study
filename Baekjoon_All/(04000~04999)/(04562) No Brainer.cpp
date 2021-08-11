218c9b5d3bfa8ed09d38e6f8a8b603f45a3333a1
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-08-11 14:57:34 +0900

    Success: 04562

---
+++ Baekjoon_All/(04000~04999)/(04562) No Brainer.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	FOR(i, 1, N) {
+		int x, y;
+		cin >> x >> y;
+		cout << (x >= y ? "MMM BRAINS" : "NO BRAINS") << "\n";
+	}
+
+
+	return 0;
+}
