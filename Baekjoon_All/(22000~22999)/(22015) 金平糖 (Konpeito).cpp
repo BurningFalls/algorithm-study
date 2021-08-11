c18edb0123db2cf61e8158a0d865e6a553424e79
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-08-11 14:58:34 +0900

    Success: 22015

---
+++ Baekjoon_All/(22000~22999)/(22015) 金平糖 (Konpeito).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	int A, B, C;
+	cin >> A >> B >> C;
+	int maxi = max({ A, B, C });
+	cout << 3 * maxi - (A + B + C);
+
+	return 0;
+}
