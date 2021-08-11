319a249dfddffe5c4defd0993f0e5efbeed916d8
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-08-11 14:57:55 +0900

    Success: 07891

---
+++ Baekjoon_All/(07000~07999)/(07891) Can you add this.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+
+int main() {
+	FASTIO;
+	int T;
+	cin >> T;
+	while (T--) {
+		int a, b;
+		cin >> a >> b;
+		cout << a + b << "\n";
+	}
+
+
+	return 0;
+}
