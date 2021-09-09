b36d6b6580fff41764a63907d6f4d6e066879017
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-09-09 17:21:42 +0900

    Updated: ReSuccess

--- Baekjoon_All/(12000~12999)/(12833) XORXORXOR.cpp
+++ Baekjoon_All/(12000~12999)/(12833) XORXORXOR.cpp
@@ -1,15 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define MEMSET(arr, num) memset(arr, num, sizeof(arr));
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+#define LEN(s) int(s.size())
+
 int main() {
+	FASTIO;
 	int A, B, C;
-	long long int temp;
 	cin >> A >> B >> C;
-	temp = A;
-	for (int i = 0; i < C; i++) {
-		temp ^= B;
-	}
-	cout << temp;
+	if (C % 2 == 0)
+		cout << A;
+	else if (C % 2 == 1)
+		cout << (A ^ B);
 
 	return 0;
 }
