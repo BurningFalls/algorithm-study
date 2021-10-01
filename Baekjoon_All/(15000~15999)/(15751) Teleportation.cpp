1978927eb02d83b6344022e0267298cc694e9140
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-10-01 13:28:29 +0900

    Success: 15751

---
+++ Baekjoon_All/(15000~15999)/(15751) Teleportation.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define MEMSET(arr, num) memset(arr, num, sizeof(arr));
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+#define LEN(s) int(s.size())
+
+int main() {
+	FASTIO;
+	int a, b, x, y;
+	cin >> a >> b >> x >> y;
+	cout << min({ abs(b - a), abs(x - a) + abs(b - y),
+		abs(y - a) + abs(b - x) });
+
+	return 0;
+}
