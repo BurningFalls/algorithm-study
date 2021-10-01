e93a25f889da52fe3265d231b92abddeb091c80d
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-10-01 13:28:24 +0900

    Success: 15232

---
+++ Baekjoon_All/(15000~15999)/(15232) Rectangles.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define MEMSET(arr, num) memset(arr, num, sizeof(arr));
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define pii pair<int, int>
+#define LEN(s) int(s.size())
+
+int main() {
+	FASTIO;
+	int N, M;
+	cin >> N >> M;
+	FOR(i, 1, N) {
+		FOR(j, 1, M) {
+			cout << '*';
+		}
+		cout << "\n";
+	}
+
+
+
+	return 0;
+}
