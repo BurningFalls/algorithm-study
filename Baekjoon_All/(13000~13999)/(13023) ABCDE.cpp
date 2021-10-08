b6269d97143d5144efbf6b86a2875d352effcd18
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-10-08 17:28:23 +0900

    Success: 13023

--- Baekjoon_All/(13000~13999)/(13023) ABCDE.cpp
+++ Baekjoon_All/(13000~13999)/(13023) ABCDE.cpp
@@ -3,65 +3,44 @@ using namespace std;
 
 typedef long long int ll;
 #define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define MEMSET(arr, num) memset(arr, num, sizeof(arr));
 #define FOR(i,a,b) for(int i=(a);i<=(b);i++)
 #define ROF(i,a,b) for(int i=(a);i>=(b);i--)
 #define pii pair<int, int>
+#define LEN(s) int(s.size())
+#define ALL(v) v.begin(),v.end()
 
-int N, M;
-vector<int> v[2001];
-int indegree[2001] = { 0, };
-bool visited[2001] = { 0, };
-bool flag;
-
-void DFS(int node, int cnt) {
-	if (flag)
-		return;
-	if (cnt == 5) {
-		flag = true;
-		return;
+void DFS(int node, int depth, vector<vector<int>>& edge, vector<bool>& visited) {
+	if (depth == 4) {
+		cout << 1;
+		exit(0);
 	}
-	int len = v[node].size();
-	int temp;
-	FOR(i, 0, len - 1) {
-		int nn = v[node][i];
-		if (visited[nn]) continue;
-		visited[nn] = 1;
-		DFS(nn, cnt + 1);
-		visited[nn] = 0;
-		if (flag)
-			break;
+	vector<int> v;
+	sort(ALL(v));
+	visited[node] = 1;
+	for (int next : edge[node]) {
+		if (visited[next]) continue;
+		DFS(next, depth + 1, edge, visited);
 	}
+	visited[node] = 0;
 }
 
 int main() {
 	FASTIO;
-	int cnt = 0;
+	int N, M;
+	vector<vector<int>> edge(2001);
+	vector<bool> visited(2001, 0);
 	cin >> N >> M;
 	FOR(i, 1, M) {
 		int a, b;
 		cin >> a >> b;
-		v[a].push_back(b);
-		v[b].push_back(a);
-		indegree[a]++;
-		indegree[b]++;
+		edge[a].push_back(b);
+		edge[b].push_back(a);
 	}
 	FOR(i, 0, N - 1) {
-		if (indegree[i] == 1)
-			cnt++;
+		DFS(i, 0, edge, visited);
 	}
-	if (cnt == 0)
-		cout << 1;
-	else {
-		FOR(i, 0, N - 1) {
-			flag = false;
-			visited[i] = 1;
-			DFS(i, 1);
-			visited[i] = 0;
-			if (flag)
-				break;
-		}
-		cout << flag;
-	}
+	cout << 0;
 
 	return 0;
 }
