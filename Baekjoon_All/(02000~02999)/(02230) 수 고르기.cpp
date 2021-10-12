dd60837c34a87713864a087e0e28c4a28aae9ef5
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-10-12 16:14:47 +0900

    Success: 02230

---
+++ Baekjoon_All/(02000~02999)/(02230) 수 고르기.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF 2e9
+#define LINF 1e18
+#define P1(a) cout << a << "\n"
+#define P2(a, b) cout << a << " " << b << "\n";
+#define P3(a, b, c) cout << a << " " << b << " " << c << "\n";
+
+int main() {
+	FASTIO;
+	int N, M;
+	cin >> N >> M;
+	vector<int> v(N);
+	FOR(i, 0, N - 1) {
+		cin >> v[i];
+	}
+	sort(ALL(v));
+	int ans = 2100000000;
+	FOR(i, 0, N - 1) {
+		if (v[N - 1] < v[i] + M)
+			break;
+		int lb = lower_bound(ALL(v), v[i] + M) - v.begin();
+		ans = min(ans, v[lb] - v[i]);
+	}
+	cout << ans;
+
+	return 0;
+}
