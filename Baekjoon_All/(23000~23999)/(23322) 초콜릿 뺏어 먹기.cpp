5f404bc4d8a5db1b01056a248dce647da749361d
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-11-01 09:14:48 +0900

    Success: 23322

---
+++ Baekjoon_All/(23000~23999)/(23322) 초콜릿 뺏어 먹기.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF 2e9
+#define LINF 1e18
+#define P1(a) cout << a << "\n"
+#define P2(a, b) cout << a << " " << b << "\n";
+
+int main() {
+	FASTIO;
+	int N, K;
+	cin >> N >> K;
+	vector<int> v(N);
+	int mini = INF;
+	FOR(i, 0, N - 1) {
+		cin >> v[i];
+		mini = min(mini, v[i]);
+	}
+	int sum = 0;
+	int cnt = 0;
+	FOR(i, 0, N - 1) {
+		sum += (v[i] - mini);
+		if (v[i] != mini) {
+			cnt++;
+		}
+	}
+	cout << sum << " " << cnt;
+
+
+	return 0;
+}
