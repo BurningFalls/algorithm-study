60ab062fdf27cbc4907e650e72b6c2298647292c
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-11-01 09:14:51 +0900

    Success: 23323

---
+++ Baekjoon_All/(23000~23999)/(23323) 황소 다마고치.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF 2e9
+#define LINF 1e18
+#define P1(a) cout << a << "\n"
+#define P2(a, b) cout << a << " " << b << "\n";
+
+int main() {
+	FASTIO;
+	int T;
+	cin >> T;
+	while (T--) {
+		ll N, M;
+		cin >> N >> M;
+		int cnt = 0;
+		while (N != 0) {
+			N /= 2;
+			cnt++;
+		}
+		cout << cnt + M << "\n";
+	}
+
+	return 0;
+}
