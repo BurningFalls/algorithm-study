dcff3bb20061ed35fa394a855370a5f905635230
BurningFalls <spdlqj7791@naver.com>
2021-11-29 08:58:57 +0900

    Success: 23739

---
+++ Baekjoon_All/(23000~23999)/(23739) 벼락치기.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF 2e9
+#define LINF 1e18
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	vector<int> T(N);
+	FOR(i, 0, N - 1) {
+		cin >> T[i];
+	}
+	int study = 30;
+	int ans = 0;
+	FOR(i, 0, N - 1) {
+		if (T[i] <= study) {
+			study -= T[i];
+			ans++;
+		}
+		else if (T[i] > study) {
+			if (T[i] <= study * 2) {
+				ans++;
+			}
+			study = 0;
+		}
+		if (study == 0) {
+			study = 30;
+		}
+	}
+	cout << ans;
+
+	return 0;
+}
