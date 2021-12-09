67f437531982e56b1be4bba6ba94e07141c6a344
BurningFalls <spdlqj7791@naver.com>
2021-12-09 10:30:23 +0900

    Success: 23808

---
+++ Baekjoon_All/(23000~23999)/(23808) 골뱅이 찍기 - ㅂ.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF 2e9
+#define LINF 1e18
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	FOR(i, 1, 2 * N) {
+		FOR(j, 1, N) {
+			cout << '@';
+		}
+		FOR(j, 1, 3 * N) {
+			cout << ' ';
+		}
+		FOR(j, 1, N) {
+			cout << '@';
+		}
+		cout << "\n";
+	}
+	FOR(i, 1, N) {
+		FOR(j, 1, 5 * N) {
+			cout << '@';
+		}
+		cout << "\n";
+	}
+	FOR(i, 1, N) {
+		FOR(j, 1, N) {
+			cout << '@';
+		}
+		FOR(j, 1, 3 * N) {
+			cout << ' ';
+		}
+		FOR(j, 1, N) {
+			cout << '@';
+		}
+		cout << "\n";
+	}
+	FOR(i, 1, N) {
+		FOR(j, 1, 5 * N) {
+			cout << '@';
+		}
+		cout << "\n";
+	}
+
+
+	return 0;
+}
