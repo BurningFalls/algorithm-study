08dd2e37edead779c7da22eadbc2b1a82adfba08
BurningFalls <spdlqj7791@naver.com>
2022-01-03 16:59:18 +0900

    Success: 23890

---
+++ Baekjoon_All/(23000~23999)/(23890) 달팽이팽이.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF 2e9
+#define LINF 1e18
+
+int main() {
+	FASTIO;
+	ll R;
+	cin >> R;
+	ll y = R - 1;
+	ll x = sqrt(2 * R - 1);
+	cout << (x * x + y * y == R * R ? x - 1 : x) << " ";
+	cout << y;
+
+	return 0;
+}
