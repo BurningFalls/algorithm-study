6734508f01e1044fbc162204fe63f144e4b00c8d
BurningFalls <spdlqj7791@naver.com>
2022-01-11 14:49:02 +0900

    Success: 24087

---
+++ Baekjoon_All/(24000~24999)/(24087) アイスクリーム (Ice Cream).cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF 2e9
+#define LINF 1e18
+
+int main() {
+	FASTIO;
+	int S, A, B;
+	cin >> S >> A >> B;
+	int tmp = max(0, S - A) / B;
+	if (max(0, S - A) % B != 0) {
+		tmp++;
+	}
+	cout << 250 + 100 * tmp;
+
+	return 0;
+}
