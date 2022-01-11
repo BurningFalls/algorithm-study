98506a24ee649d792087eabe06347e30a17e8943
BurningFalls <spdlqj7791@naver.com>
2022-01-11 14:48:05 +0900

    Success: 20673

--- Baekjoon_All/(20000~20999)/(20673) Covid-19.cpp
+++ Baekjoon_All/(20000~20999)/(20673) Covid-19.cpp
@@ -1,22 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long int ll;
 #define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
 #define FOR(i,a,b) for(int i=(a);i<=(b);i++)
 #define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
 #define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF 2e9
+#define LINF 1e18
 
 int main() {
 	FASTIO;
-	int a, b;
-	cin >> a >> b;
-	if (a <= 50 && b <= 10)
+	int p, q;
+	cin >> p >> q;
+	if (p <= 50 && q <= 10) {
 		cout << "White";
-	else if (b >= 30)
+	}
+	else if (q > 30) {
 		cout << "Red";
-	else
+	}
+	else {
 		cout << "Yellow";
+	}
 
 
 	return 0;
