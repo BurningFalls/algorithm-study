c9c2d6e50d0bcfbcf4394e8da7d2e2be75e7b258
BurningFalls <spdlqj7791@naver.com>
2022-01-11 17:20:47 +0900

    Success: 18856

---
+++ Baekjoon_All/(18000~18999)/(18856) 피드백.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF 2e9
+#define LINF 1e18
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	cout << N << "\n";
+	FOR(i, 1, N - 1) {
+		cout << i << " ";
+	}
+	cout << 53;
+
+	return 0;
+}
