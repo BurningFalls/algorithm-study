43ac084ac9038828904b34bd2db3cbfe4c6a5999
BurningFalls <spdlqj7791@naver.com>
2022-03-22 17:23:08 +0900

    Contest: Baekjoon Contest

---
+++ Baekjoon_Study (2022)/03월/(03.20) 2022 연세대학교 신학기맞이 프로그래밍 경진대회/(24724) 현대모비스와 함께하는 부품 관리.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF 2e9
+#define LINF 1e18
+
+int main() {
+	FASTIO;
+	int T;
+	cin >> T;
+	FOR(t, 1, T) {
+		int A, B, N;
+		cin >> N;
+		cin >> A >> B;
+		FOR(i, 1, N) {
+			int u, v;
+			cin >> u >> v;
+		}
+		cout << "Material Management " << t << "\n";
+		cout << "Classification ---- End!" << "\n";
+	}
+
+	return 0;
+}
