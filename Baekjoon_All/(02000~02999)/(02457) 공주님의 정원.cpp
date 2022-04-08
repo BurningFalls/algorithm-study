7dc3285859016108a591526e98d88011ab955e96
BurningFalls <spdlqj7791@naver.com>
2022-04-08 16:39:37 +0900

    Success: 02457

--- Baekjoon_All/(02000~02999)/(02457) 공주님의 정원.cpp
+++ Baekjoon_All/(02000~02999)/(02457) 공주님의 정원.cpp
@@ -6,68 +6,41 @@ using namespace std;
 #define ROF(i,a,b) for(int i=(a);i>=(b);i--)
 #define ll long long int
 #define pii pair<int, int>
-#define vi vector<int>
-#define vvi vector<vector<int>>
 #define PQ priority_queue
 #define LEN(v) int(v.size())
 #define ALL(v) v.begin(),v.end()
 #define INF 2e9
 #define LINF 1e18
-#define P1(a) cout << a << "\n"
-#define P2(a, b) cout << a << " " << b << "\n";
-#define P3(a, b, c) cout << a << " " << b << " " << c << "\n";
-
-struct NODE {
-	int month;
-	int day;
-};
-
-bool operator<(NODE a, NODE b) {
-	if (a.month == b.month)
-		return a.day < b.day;
-	return a.month < b.month;
-}
-
-bool operator==(NODE a, NODE b) {
-	return a.month == b.month && a.day == b.day;
-}
-
-bool compare(pair<NODE, NODE> a, pair<NODE, NODE> b) {
-	if (a.first == b.first)
-		return a.second < b.second;
-	return a.first < b.first;
-}
 
 int main() {
 	FASTIO;
 	int N;
 	cin >> N;
-	vector<pair<NODE, NODE>> v(N);
+	vector<pii> v;
 	FOR(i, 0, N - 1) {
-		cin >> v[i].first.month >> v[i].first.day;
-		cin >> v[i].second.month >> v[i].second.day;
+		int a, b, c, d;
+		cin >> a >> b >> c >> d;
+		int start = 100 * a + b;
+		int end = 100 * c + d;
+		if (start >= end) continue;
+		v.push_back({ start, end });
 	}
-	sort(ALL(v), compare);
-	NODE end = { 3, 1 };
-	NODE final = { 11, 31 };
+	sort(ALL(v));
+
+	int right = 301;
 	int idx = 0;
-	int ans = 0;
-	if (end < v[0].first) {
-		cout << 0;
-		return 0;
-	}
-	while (idx <= N - 1 && end < final) {
-		NODE max_end = { 0, 0 };
-		while (idx <= N - 1 && (v[idx].first < end || v[idx].first == end)) {
-			if (max_end < v[idx].second) {
-				max_end = v[idx].second;
-			}
+	int cnt = 0;
+	while (idx < N && right <= 1130) {
+		int nright = 0;
+		while (idx < N && v[idx].first <= right) {
+			nright = max(nright, v[idx].second);
 			idx++;
 		}
-		ans++;
-		end = max_end;
+		if (nright == 0) break;
+		right = nright;
+		cnt++;
 	}
-	cout << (end < final ? 0 : ans);
+	cout << (right > 1130 ? cnt : 0);
 
 	return 0;
 }
