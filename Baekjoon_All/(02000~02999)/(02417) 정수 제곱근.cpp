367283c0d25736826ff672cce96469766698cff3
BurningFalls <spdlqj7791@naver.com>
2022-04-08 16:39:32 +0900

    Success: 02417

--- Baekjoon_All/(02000~02999)/(02417) 정수 제곱근.cpp
+++ Baekjoon_All/(02000~02999)/(02417) 정수 제곱근.cpp
@@ -1,20 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long int ll;
 #define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
 #define FOR(i,a,b) for(int i=(a);i<=(b);i++)
 #define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
 #define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF 2e9
+#define LINF 1e18
 
 int main() {
 	FASTIO;
-	ll N;
-	long double q;
-	cin >> N;
-	q = sqrt(N);
-	N = ceil(q);
-	cout << N;
+	ll n;
+	cin >> n;
+	ll sqrtn = sqrtl(n);
+	cout << ((sqrtn * sqrtn >= n) ? sqrtn : sqrtn + 1);
+
 
 	return 0;
 }
