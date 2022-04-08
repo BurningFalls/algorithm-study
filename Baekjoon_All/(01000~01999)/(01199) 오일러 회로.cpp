f65b49e460dfc521ff49db4dc58bf45949e6ad34
BurningFalls <spdlqj7791@naver.com>
2022-04-08 13:31:35 +0900

    Success: 01199

--- Baekjoon_All/(01000~01999)/(01199) 오일러 회로.cpp
+++ Baekjoon_All/(01000~01999)/(01199) 오일러 회로.cpp
@@ -1,52 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long int ll;
 #define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
 #define FOR(i,a,b) for(int i=(a);i<=(b);i++)
 #define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
 #define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF 2e9
+#define LINF 1e18
+#define MAX 1001
 
 int N;
-int Graph[1001][1001];
-int conn[1001] = { 0, };
+vector<vector<pii>> Edge(MAX, vector<pii>());
+vector<vector<int>> edge_cnt(MAX, vector<int>(MAX));
 
 void DFS(int node) {
-	FOR(i, 1, N) {
-		while (Graph[node][i]) {
-			Graph[node][i]--;
-			Graph[i][node]--;
-			DFS(i);
+	while (!Edge[node].empty()) {
+		while (!Edge[node].empty() && edge_cnt[node][Edge[node].back().first] == 0) {
+			Edge[node].pop_back();
 		}
+		if (Edge[node].empty()) break;
+		pii next = Edge[node].back();
+		if (--Edge[node].back().second == 0) {
+			Edge[node].pop_back();
+		}
+		edge_cnt[node][next.first]--;
+		edge_cnt[next.first][node]--;
+		DFS(next.first);
 	}
 	cout << node << " ";
 }
 
 int main() {
 	FASTIO;
+	bool flag = true;
 	cin >> N;
 	FOR(i, 1, N) {
+		int cnt = 0;
 		FOR(j, 1, N) {
-			cin >> Graph[i][j];
-			conn[i] += Graph[i][j];
-			conn[j] += Graph[i][j];
+			int num;
+			cin >> num;
+			edge_cnt[i][j] += num;
+			cnt += num;
+			if (num != 0) {
+				Edge[i].push_back({ j, num });
+			}
 		}
-	}
-	FOR(i, 1, N)
-		conn[i] /= 2;
-	bool flag = true;
-	FOR(i, 1, N) {
-		if (conn[i] % 2 == 1) {
+		if (cnt % 2 == 1) {
 			flag = false;
-			break;
 		}
 	}
-	if (!flag)
+	if (!flag) {
 		cout << -1;
+	}
 	else {
 		DFS(1);
 	}
-
 
 	return 0;
 }
