ba920e0e714073d3f935e6354c8bdec209d3c829
BurningFalls <spdlqj7791@naver.com>
2022-04-11 13:05:30 +0900

    Success: 10158

--- Baekjoon_All/(10000~10999)/(10158) 개미.cpp
+++ Baekjoon_All/(10000~10999)/(10158) 개미.cpp
@@ -1,50 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long int ll;
 #define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
 #define FOR(i,a,b) for(int i=(a);i<=(b);i++)
 #define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
 #define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF 2e9
+#define LINF 1e18
 
-int W, H;
-int P, Q, T;
-int Dir;
-int dy[4] = { 1, 1, -1, -1 };
-int dx[4] = { 1, -1, -1, 1 };
-
-bool Move() {
-	int dist1, dist2;
-	int arr[4] = { W - P, H - Q, P, Q };
-	dist1 = arr[Dir];
-	dist2 = arr[(Dir + 1) % 4];
-	int moving = min(dist1, dist2);
-	T -= moving;
-	P += dx[Dir] * moving;
-	Q += dy[Dir] * moving;
-	if (T <= 0)
-		return false;
-	if (dist1 < dist2)
-		Dir = (Dir + 1) % 4;
-	else if (dist1 > dist2)
-		Dir = (Dir + 3) % 4;
-	else if (dist1 == dist2)
-		Dir = (Dir + 2) % 4;
-	return true;
+bool operator==(pii a, pii b) {
+	return a.first == b.first && a.second == b.second;
 }
 
 int main() {
 	FASTIO;
+	int W, H, x, y, T;
 	cin >> W >> H;
-	cin >> P >> Q;
+	cin >> x >> y;
 	cin >> T;
-	Dir = 0;
-	while (Move())
-		;
-	Dir = (Dir + 2) % 4;
-	P += dx[Dir] * (0 - T);
-	Q += dy[Dir] * (0 - T);
-	cout << P << " " << Q;
+
+	int cnt = 0;
+	pii pos = { y, x };
+	pii dir = { 1, 1 };
+	pii d[4] = { {1, 1}, {1, -1}, {-1, 1}, {-1, -1} };
+	pii limit[4] = { {H, W}, {H, 0}, {0, W}, {0, 0} };
+	bool flag = true;
+	while (flag) {
+		FOR(i, 0, 3) {
+			if (dir == d[i]) {
+				if (i == 0 && pos.first - pos.second == y - x && pos.first <= y && pos.second <= x && cnt != 0) {
+					cnt += y - pos.first;
+					flag = false;
+					break;
+				}
+				int tmp = min({ abs(limit[i].first - pos.first), abs(limit[i].second - pos.second) });
+				cnt += tmp;
+				pos.first += dir.first * tmp;
+				pos.second += dir.second * tmp;
+				if (pos.first == H || pos.first == 0) dir.first *= (-1);
+				if (pos.second == W || pos.second == 0) dir.second *= (-1);
+				break;
+			}
+		}
+	}
+	T %= cnt;
+
+	pos = { y, x };
+	dir = { 1, 1 };
+	flag = true;
+	while (flag) {
+		FOR(i, 0, 3) {
+			if (dir == d[i]) {
+				int tmp = min({ abs(limit[i].first - pos.first), abs(limit[i].second - pos.second) });
+				if (T < tmp) {
+					pos.first += dir.first * T;
+					pos.second += dir.second * T;
+					flag = false;
+					break;
+				}
+				T -= tmp;
+				pos.first += dir.first * tmp;
+				pos.second += dir.second * tmp;
+				if (pos.first == H || pos.first == 0) dir.first *= (-1);
+				if (pos.second == W || pos.second == 0) dir.second *= (-1);
+				break;
+			}
+		}
+	}
+
+	cout << pos.second << " " << pos.first;
 
 	return 0;
 }
