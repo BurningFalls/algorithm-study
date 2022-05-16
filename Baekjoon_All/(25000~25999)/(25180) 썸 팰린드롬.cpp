d8fe7b8a23400e4045288d2ca7fd78d7eb1489b6
BurningFalls <spdlqj7791@naver.com>
2022-05-16 09:16:03 +0900

    Success: 25180

---
+++ Baekjoon_All/(25000~25999)/(25180) 썸 팰린드롬.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF 2e9
+#define LINF 1e18
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+
+	int ans = (N - 1) / 9 + 1;
+	if (ans % 2 == 0 && N % 2 == 1) {
+		ans += 1;
+	}
+	cout << ans;
+
+	return 0;
+}
