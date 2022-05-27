d5465b2e61378960cb8921536e3fbecc083d258f
BurningFalls <sherlocky31415@gmail.com>
2022-05-27 16:44:40 +0900

    Modified: 02696

--- Baekjoon_All/(02000~02999)/(02696) 중앙값 구하기.cpp
+++ Baekjoon_All/(02000~02999)/(02696) 중앙값 구하기.cpp
@@ -1,63 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FASTIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-#define FOR(i, a, b) for(int i=a;i<=b;i++)
-#define ROF(i, a, b) for(int i=a;i>=b;i--)
-#define pii pair<int, int>
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
 #define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF int(2e9)
+#define LINF ll(1e18)
 
 int main() {
 	FASTIO;
 	int T;
 	cin >> T;
 	while (T--) {
-		vector<int> ans_v;
-		priority_queue<int> pq1, pq2;
+		PQ<int> pq1;
+		PQ<int, vector<int>, greater<int>> pq2;
+		vector<int> ans;
 		int M;
-		int num;
-		int len1 = 0, len2 = 0;
 		cin >> M;
 		FOR(i, 1, M) {
+			int num;
 			cin >> num;
-			if (i == 1) {
+			if (pq1.empty() || pq1.top() >= num) {
 				pq1.push(num);
-				ans_v.push_back(num);
-				len1++;
-				continue;
 			}
-			if (pq1.top() < num) {
-				pq2.push(-num);
-				len2++;
+			else {
+				pq2.push(num);
 			}
-			else if (pq1.top() >= num) {
-				pq1.push(num);
-				len1++;
+			if (LEN(pq1) < LEN(pq2)) {
+				pq1.push(pq2.top());
+				pq2.pop();
+			}
+			if (LEN(pq1) > LEN(pq2) + 1) {
+				pq2.push(pq1.top());
+				pq1.pop();
 			}
 			if (i % 2 == 1) {
-				if (len1 == len2 + 3) {
-					pq2.push(-pq1.top());
-					pq1.pop();
-					len1--;
-					len2++;
-				}
-				else if (len1 + 1 == len2) {
-					pq1.push(-pq2.top());
-					pq2.pop();
-					len1++;
-					len2--;
-				}
-				ans_v.push_back(pq1.top());
+				ans.push_back(pq1.top());
 			}
 		}
-		int len = ans_v.size();
-		cout << len << "\n";
-		FOR(i, 0, len - 1) {
-			if (i != 0 && i % 10 == 0)
+		cout << LEN(ans) << "\n";
+		FOR(i, 0, LEN(ans) - 1) {
+			cout << ans[i] << " ";
+			if (i % 10 == 9) {
 				cout << "\n";
-			cout << ans_v[i] << " ";
+			}
 		}
-		cout << "\n";
+		if ((LEN(ans) - 1) % 10 != 9) {
+			cout << "\n";
+		}
 	}
 
 	return 0;
