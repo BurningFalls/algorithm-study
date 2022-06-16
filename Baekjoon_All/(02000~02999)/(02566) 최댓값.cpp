5314d284be7cd2ec90000d4dd16d666c2276cf79
BurningFalls <sherlocky31415@gmail.com>
2022-06-16 17:00:18 +0900

    [UPDATE] (02566) 최댓값.cpp 재채점 후 WA를 받아 다시 맞게 수정

--- Baekjoon_All/(02000~02999)/(02566) 최댓값.cpp
+++ Baekjoon_All/(02000~02999)/(02566) 최댓값.cpp
@@ -1,25 +1,37 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) int(v.size())
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
 int main() {
-	int num;
-	int maximum;
-	int yloc, xloc;
-	for (int i = 0; i < 9; i++) {
-		for (int j = 0; j < 9; j++) {
-			cin >> num;
-			if (i == 0 && j == 0)
-				maximum = num;
-			else {
-				if (maximum < num) {
-					maximum = num;
-					yloc = i + 1;
-					xloc = j + 1;
-				}
+	FASTIO;
+	int maxi = 0;
+	vector<vector<int>> v(10, vector<int>(10));
+	FOR(i, 1, 9) {
+		FOR(j, 1, 9) {
+			cin >> v[i][j];
+			maxi = max(maxi, v[i][j]);
+		}
+	}
+	int y = 1, x = 1;
+	FOR(i, 1, 9) {
+		FOR(j, 1, 9) {
+			if (v[i][j] == maxi) {
+				y = i, x = j;
 			}
 		}
 	}
-	cout << maximum << "\n" << yloc << " " << xloc;
+	cout << maxi << "\n";
+	cout << y << " " << x;
 
 	return 0;
 }
