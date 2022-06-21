879add4323a1002035a8df50b046b88ef2d5958f
BurningFalls <sherlocky31415@gmail.com>
2022-06-21 09:54:47 +0900

    [CREATE] baekjoon 23235번

---
+++ Baekjoon_All/(23000~23999)/(23235) The Fastest Sorting Algorithm In The World.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int t = 1;
+	while (true) {
+		int N;
+		cin >> N;
+		if (N == 0) break;
+		FOR(i, 1, N) {
+			int num;
+			cin >> num;
+		}
+		cout << "Case " << t << ": Sorting... done!\n";
+		t++;
+	}
+
+	return 0;
+}
