3cc758a54308975d409fa7deb078f3d4d28a9382
BurningFalls <sherlocky31415@gmail.com>
2022-06-21 09:54:41 +0900

    [CREATE] baekjoon 18409번

---
+++ Baekjoon_All/(18000~18999)/(18409) 母音を数える (Counting Vowels).cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int N;
+	string s;
+	cin >> N;
+	cin >> s;
+	int cnt = 0;
+	FOR(i, 0, N - 1) {
+		if (s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u') continue;
+		cnt++;
+	}
+	cout << cnt;
+
+	return 0;
+}
