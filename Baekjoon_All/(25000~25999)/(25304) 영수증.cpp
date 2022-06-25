ece864c70de2908194fef8d7222d00fae19c1e5a
BurningFalls <sherlocky31415@gmail.com>
2022-06-25 21:29:46 +0900

    [CREATE] BOJ 25304

---
+++ Baekjoon_All/(25000~25999)/(25304) 영수증.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	ll X, N, a, b;
+	cin >> X >> N;
+	ll sum = 0;
+	FOR(i, 1, N) {
+		cin >> a >> b;
+		sum += a * b;
+	}
+	cout << (X == sum ? "Yes" : "No");
+
+	return 0;
+}
