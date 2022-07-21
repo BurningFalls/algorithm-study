d59582c4643aab276d77633bac55c84223322d97
BurningFalls <sherlocky31415@gmail.com>
2022-07-21 17:17:54 +0900

    [CREATE] BOJ 25377

---
+++ Baekjoon_All/(25000~25999)/(25377) 빵.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	int A, B;
+	int mini = INF;
+	FOR(i, 1, N) {
+		cin >> A >> B;
+		if (A > B) continue;
+		mini = min(mini, B);
+	}
+	cout << (mini == INF ? -1 : mini);
+
+	return 0;
+}
