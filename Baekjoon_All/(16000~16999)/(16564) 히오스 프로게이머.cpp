22a3fdd050fe407b28c734a5496ea47f1ee9778f
BurningFalls <sherlocky31415@gmail.com>
2022-08-08 13:23:00 +0900

    [UPDATE] BOJ 16564

--- Baekjoon_All/(16000~16999)/(16564) 히오스 프로게이머.cpp
+++ Baekjoon_All/(16000~16999)/(16564) 히오스 프로게이머.cpp
@@ -1,37 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long int ll;
 #define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
 #define FOR(i,a,b) for(int i=(a);i<=(b);i++)
 #define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
 #define pii pair<int, int>
-
-ll X[1000001];
-ll SUM[1000001];
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
 
 int main() {
 	FASTIO;
 	int N, K;
 	cin >> N >> K;
-	SUM[0] = 0;
-	FOR(i, 1, N)
+	vector<int> X(N);
+	int mini = INF;
+	int maxi = 0;
+	FOR(i, 0, N - 1) {
 		cin >> X[i];
-	sort(X + 1, X + N + 1);
-	FOR(i, 1, N)
-		SUM[i] = SUM[i - 1] + X[i];
-	int left = X[1], right = X[N];
-	int ans = 0;
+		mini = min(mini, X[i]);
+		maxi = max(maxi, X[i]);
+	}
+	int left = mini, right = maxi + K;
+	int ans = mini;
 	while (left <= right) {
 		int mid = (left + right) / 2;
-		int lb = lower_bound(X + 1, X + N + 1, mid) - X;
-		ll sum = (ll)mid * ((ll)lb - 1) - SUM[lb - 1];
+		ll sum = 0;
+		FOR(i, 0, N - 1) {
+			sum += max(0, mid - X[i]);
+		}
 		if (sum <= K) {
 			ans = max(ans, mid);
 			left = mid + 1;
 		}
-		else
+		else if (sum > K) {
 			right = mid - 1;
+		}
 	}
 	cout << ans;
 
