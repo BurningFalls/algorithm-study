2dc333d40c336a69e461b1a2648b0420873ca359
BurningFalls <sherlocky31415@gmail.com>
2022-08-11 17:09:33 +0900

    [CREATE] BOJ 15151

---
+++ Baekjoon_All/(15000~15999)/(15151) Incomplete Book.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	ll k, d;
+	cin >> k >> d;
+	int cnt = 1;
+	int num = 2;
+	while ((num - 1) * k <= d) {
+		num *= 2;
+		cnt++;
+	}
+	cout << cnt - 1;
+
+	return 0;
+}
