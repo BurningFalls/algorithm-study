e703eb288feb7638a66e91a4fe3b3949c1f1d74c
BurningFalls <sherlocky31415@gmail.com>
2022-08-16 16:58:14 +0900

    [CREATE] BOJ 22279

---
+++ Baekjoon_All/(22000~22999)/(22279) Quality-Adjusted Life-Year.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	double sum = 0;
+	FOR(i, 1, N) {
+		double a, b;
+		cin >> a >> b;
+		sum += a * b;
+	}
+	cout << fixed << setprecision(3);
+	cout << sum;
+
+	return 0;
+}
