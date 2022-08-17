db11726acc54a524a893c14ce0ed0b24828bd4cc
BurningFalls <sherlocky31415@gmail.com>
2022-08-17 11:15:01 +0900

    [CREATE] BOJ 20674

---
+++ Baekjoon_All/(20000~20999)/(20674) Statistics.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	int mini = INF;
+	int sum = 0;
+	FOR(i, 1, N) {
+		int num;
+		cin >> num;
+		sum += max(0, num - mini);
+		mini = min(mini, num);
+	}
+	cout << sum;
+
+	return 0;
+}
