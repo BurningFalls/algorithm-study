10a06515ebe0f9add3643da92d1e2004c4cbb6d8
BurningFalls <sherlocky31415@gmail.com>
2022-08-17 11:14:50 +0900

    [CREATE] BOJ 19774

---
+++ Baekjoon_All/(19000~19999)/(19774) ABCD-код.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	FOR(i, 1, N) {
+		int num;
+		cin >> num;
+		int a = num / 100;
+		int b = num % 100;
+		cout << ((a * a + b * b) % 7 == 1 ? "YES" : "NO") << "\n";
+	}
+
+	return 0;
+}
