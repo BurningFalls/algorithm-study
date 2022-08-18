0c939da37c88ec53f0f76a758a843004ddbfabeb
BurningFalls <sherlocky31415@gmail.com>
2022-08-18 10:32:18 +0900

    [CREATE] BOJ 24408

---
+++ Baekjoon_All/(24000~24999)/(24408) Mult!.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	vector<int> v(N);
+	FOR(i, 0, N - 1) {
+		cin >> v[i];
+	}
+	int num = -1;
+	FOR(i, 0, N - 1) {
+		if (num == -1) {
+			num = v[i];
+		}
+		else {
+			if (v[i] % num == 0) {
+				cout << v[i] << "\n";
+				num = -1;
+			}
+		}
+	}
+
+	return 0;
+}
