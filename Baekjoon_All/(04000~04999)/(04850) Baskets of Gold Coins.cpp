f146eee1a96a25ce3379f21d3fb0b3bcd08ab79d
BurningFalls <sherlocky31415@gmail.com>
2022-08-19 10:35:43 +0900

    [CREATE] BOJ 04850

---
+++ Baekjoon_All/(04000~04999)/(04850) Baskets of Gold Coins.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int n, w, d, sum;
+	while (cin >> n >> w >> d >> sum) {
+		int ans = (n * (n - 1) / 2 * w - sum) / d;
+		if (ans == 0) ans = n;
+		cout << ans << "\n";
+	}
+
+	return 0;
+}
