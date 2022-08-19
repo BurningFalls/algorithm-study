593ee98568bb6030346c2f3803dd600fc034731e
BurningFalls <sherlocky31415@gmail.com>
2022-08-19 10:35:59 +0900

    [CREATE] BOJ 25270

---
+++ Baekjoon_All/(25000~25999)/(25270) 99 Problems.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	if (N % 100 >= 49) {
+		cout << (N / 100) * 100 + 99;
+	}
+	else if (N % 100 <= 48) {
+		cout << max(99, (N / 100) * 100 - 1);
+	}
+
+	return 0;
+}
