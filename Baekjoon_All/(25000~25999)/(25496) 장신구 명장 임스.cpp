a712db9eaf79b311da0cb686a5272530c6fd5b36
BurningFalls <sherlocky31415@gmail.com>
2022-08-24 14:31:30 +0900

    [CREATE] BOJ 25496

---
+++ Baekjoon_All/(25000~25999)/(25496) 장신구 명장 임스.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int P, N;
+	cin >> P >> N;
+	vector<int> A(N);
+	FOR(i, 0, N - 1) {
+		cin >> A[i];
+	}
+	sort(ALL(A));
+	int sum = P;
+	int ans = 0;
+	FOR(i, 0, N - 1) {
+		if (sum >= 200) break;
+		ans = i + 1;
+		sum += A[i];
+	}
+	cout << ans;
+
+	return 0;
+}
