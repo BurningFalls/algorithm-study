b87a775d3d18696b8093293603b6fb03fb38dfc6
BurningFalls <sherlocky31415@gmail.com>
2022-09-26 09:23:04 +0900

    [CREATE] BOJ 25642

---
+++ Baekjoon_All/(25000~25999)/(25642) 젓가락 게임.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int A, B;
+	cin >> A >> B;
+	while (A < 5 && B < 5) {
+		B += A;
+		if (A >= 5 || B >= 5) break;
+		A += B;
+	}
+	cout << (A >= 5 ? "yj" : "yt");
+
+	return 0;
+}
