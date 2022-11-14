2f0c2f6f5fca763216256d8a93f3e0817bf6c954
BurningFalls <sherlocky31415@gmail.com>
2022-11-14 15:22:59 +0900

    [CREATE] BOJ 25972

---
+++ Baekjoon_All/(25000~25999)/(25972) 도미노 무너트리기.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	vector<pii> v(N);
+	FOR(i, 0, N - 1) {
+		cin >> v[i].first >> v[i].second;
+	}
+	sort(ALL(v));
+
+	int cnt = 1;
+	FOR(i, 1, N - 1) {
+		cnt += v[i - 1].first + v[i - 1].second < v[i].first;
+	}
+	cout << cnt;
+
+	return 0;
+}
