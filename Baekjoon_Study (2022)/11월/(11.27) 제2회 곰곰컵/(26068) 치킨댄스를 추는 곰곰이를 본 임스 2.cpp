9dd1b43f34d77aa3826e3bf9d018cde0a9d032bb
BurningFalls <sherlocky31415@gmail.com>
2022-11-27 17:56:30 +0900

    Baekjoon Contest: 제2회 곰곰컵

---
+++ Baekjoon_Study (2022)/11월/(11.27) 제2회 곰곰컵/(26068) 치킨댄스를 추는 곰곰이를 본 임스 2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	int cnt = 0;
+	FOR(i, 1, N) {
+		string s;
+		cin >> s;
+		int x = stoi(s.substr(2, LEN(s) - 2));
+		cnt += (x <= 90);
+	}
+	cout << cnt;
+
+	return 0;
+}
