af713e54fa0bd2eccaafa5fa3cc65a65582d1c6f
BurningFalls <sherlocky31415@gmail.com>
2022-12-04 13:15:49 +0900

    [CREATE] BOJ 22155

---
+++ Baekjoon_All/(22000~22999)/(22155) Простая задача.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	FOR(i, 1, N) {
+		int a, b;
+		cin >> a >> b;
+		cout << (((a <= 1 && b <= 2) || (a <= 2 && b <= 1)) ? "Yes" : "No") << "\n";
+	}
+
+	return 0;
+}
