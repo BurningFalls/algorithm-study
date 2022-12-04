c488117d71fc5c3eb13aca008b603649d50290b5
BurningFalls <sherlocky31415@gmail.com>
2022-12-04 13:16:01 +0900

    [CREATE] BOJ 25784

---
+++ Baekjoon_All/(25000~25999)/(25784) Easy-to-Solve Expressions.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	vector<int> v(3);
+	FOR(i, 0, 2) {
+		cin >> v[i];
+	}
+	sort(ALL(v));
+	if (v[0] + v[1] == v[2]) {
+		cout << 1;
+	}
+	else if (v[0] * v[1] == v[2]) {
+		cout << 2;
+	}
+	else {
+		cout << 3;
+	}
+
+	return 0;
+}
