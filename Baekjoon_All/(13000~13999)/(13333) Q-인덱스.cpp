7cbebc148332bdef29b452a76c4b72efcdc8da81
BurningFalls <sherlocky31415@gmail.com>
2022-12-07 15:19:20 +0900

    [CREATE] BOJ 13333

---
+++ Baekjoon_All/(13000~13999)/(13333) Q-인덱스.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	vector<int> v(N);
+	int zero = 0;
+	FOR(i, 0, N - 1) {
+		cin >> v[i];
+		zero += (v[i] == 0);
+	}
+	sort(ALL(v));
+	if (zero == N) {
+		cout << 0;
+		return 0;
+	}
+	FOR(k, 1, N - 1) {
+		if (v[N - k] >= k && v[N - k - 1] <= k) {
+			cout << k;
+			return 0;
+		}
+	}
+	cout << N;
+
+	return 0;
+}
