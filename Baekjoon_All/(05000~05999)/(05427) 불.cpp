2135814c4072b1d08ce804a13247f94f57d1248e
BurningFalls <sherlocky31415@gmail.com>
2022-12-13 11:29:10 +0900

    [UPDATE] BOJ 05427

--- Baekjoon_All/(05000~05999)/(05427) 불.cpp
+++ Baekjoon_All/(05000~05999)/(05427) 불.cpp
@@ -1,58 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long int ll;
 #define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
 #define FOR(i,a,b) for(int i=(a);i<=(b);i++)
 #define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
 #define pii pair<int, int>
-#define MAX 1000 + 1
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+struct NODE {
+	int y;
+	int x;
+	int vis;
+};
 
 int W, H;
-char Map[MAX][MAX];
-bool visited[MAX][MAX];
-int dy[4] = { -1, 1, 0, 0 };
-int dx[4] = { 0, 0, -1, 1 };
-queue<pii> player_q;
-queue<pii> fire_q;
+vector<vector<char>> Map;
+vector<vector<int>> visited;
+vector<int> dy = { -1, 1, 0, 0 };
+vector<int> dx = { 0, 0, -1, 1 };
+int sy, sx;
+int mini;
 
-void Spread_Fire() {
-	int len = fire_q.size();
-	FOR(i, 1, len) {
-		int y = fire_q.front().first;
-		int x = fire_q.front().second;
-		fire_q.pop();
-		FOR(j, 0, 3) {
-			int ny = y + dy[j];
-			int nx = x + dx[j];
-			if (ny < 1 || nx < 1 || ny > H || nx > W) continue;
-			if (Map[ny][nx] != '@' && visited[ny][nx]) continue;
-			visited[ny][nx] = 1;
-			fire_q.push({ ny, nx });
+void Move() {
+	queue<NODE> q;
+	FOR(i, 0, H - 1) {
+		FOR(j, 0, W - 1) {
+			if (Map[i][j] == '*') {
+				visited[i][j] = -1;
+				q.push({ i, j, -1 });
+			}
 		}
 	}
-}
+	visited[sy][sx] = 1;
+	q.push({ sy, sx, 1 });
 
-int Move_Player() {
-	bool flag = false;
-	int len = player_q.size();
-	FOR(i, 1, len) {
-		int y = player_q.front().first;
-		int x = player_q.front().second;
-		player_q.pop();
-		if (y == 1 || y == H || x == 1 || x == W)
-			return 2;
-		FOR(j, 0, 3) {
-			int ny = y + dy[j];
-			int nx = x + dx[j];
-			if (ny < 1 || nx < 1 || ny > H || nx > W) continue;
-			if (Map[ny][nx] != '@' && visited[ny][nx]) continue;
-			visited[ny][nx] = 1;
-			player_q.push({ ny, nx });
-			flag = true;
+	while (!q.empty()) {
+		int y = q.front().y;
+		int x = q.front().x;
+		int vis = q.front().vis;
+		q.pop();
+		if (vis > 0 && (y == 0 || y == H - 1 || x == 0 || x == W - 1)) {
+			mini = min(mini, vis);
+		}
+		FOR(i, 0, 3) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if (ny < 0 || nx < 0 || ny >= H || nx >= W) continue;
+			if (Map[ny][nx] == '#') continue;
+			if (vis == -1) {
+				if (visited[ny][nx] == -1) continue;
+				visited[ny][nx] = -1;
+				q.push({ ny, nx, -1 });
+			}
+			else if (vis > 0) {
+				if (visited[ny][nx] > 0) continue;
+				if (visited[ny][nx] == -1) continue;
+				visited[ny][nx] = vis + 1;
+				q.push({ ny, nx, visited[ny][nx] });
+			}
 		}
 	}
-	return flag;
 }
 
 int main() {
@@ -60,40 +72,26 @@ int main() {
 	int T;
 	cin >> T;
 	while (T--) {
-		while (!player_q.empty()) player_q.pop();
-		while (!fire_q.empty()) fire_q.pop();
-		memset(visited, 0, sizeof(visited));
 		cin >> W >> H;
-		FOR(i, 1, H) {
-			FOR(j, 1, W) {
+		Map = vector<vector<char>>(H, vector<char>(W));
+		visited = vector<vector<int>>(H, vector<int>(W, 0));
+		mini = INF;
+		FOR(i, 0, H - 1) {
+			FOR(j, 0, W - 1) {
 				cin >> Map[i][j];
-				if (Map[i][j] == '@')
-					player_q.push({ i, j });
-				else if (Map[i][j] == '*')
-					fire_q.push({ i, j });
-				if (Map[i][j] != '.')
-					visited[i][j] = 1;
+				if (Map[i][j] == '@') {
+					sy = i;
+					sx = j;
+				}
 			}
 		}
-		int flag = false;
-		int cnt = 0;
-		while (true) {
-			Spread_Fire();
-			int temp = Move_Player();
-			cnt++;
-			if (temp == 2) {
-				flag = true;
-				break;
-			}
-			else if (temp == 0) {
-				flag = false;
-				break;
-			}
-		}
-		if (flag)
-			cout << cnt;
-		else
+		Move();
+		if (mini == INF) {
 			cout << "IMPOSSIBLE";
+		}
+		else {
+			cout << mini;
+		}
 		cout << "\n";
 	}
 
