bebbbf02a6889d44496fcd5a63b2c6fca3473d3c
BurningFalls <sherlocky31415@gmail.com>
2022-12-16 10:01:13 +0900

    [CREATE] BOJ 06840

---
+++ Baekjoon_All/(06000~06999)/(06840) Who is in the middle.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	vector<int> v(3);
+	FOR(i, 0, 2) {
+		cin >> v[i];
+	}
+	sort(ALL(v));
+	cout << v[1];
+
+	return 0;
+}
