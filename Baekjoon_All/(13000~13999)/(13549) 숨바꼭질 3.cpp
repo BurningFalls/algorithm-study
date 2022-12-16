1099d7b50a1b074634bc628716b1e2891a2662be
BurningFalls <sherlocky31415@gmail.com>
2022-12-17 00:50:17 +0900

    [UPDATE] BOJ 13549

--- Baekjoon_All/(13000~13999)/(13549) 숨바꼭질 3.cpp
+++ Baekjoon_All/(13000~13999)/(13549) 숨바꼭질 3.cpp
@@ -1,43 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long int ll;
 #define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
 #define FOR(i,a,b) for(int i=(a);i<=(b);i++)
 #define ROF(i,a,b) for(int i=(a);i>=(b);i--)
-
-int N, K;
-bool visited[100001] = { 0, };
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-int BFS() {
-	pq.push(make_pair(1, N));
-	visited[N] = 1;
-	while (!pq.empty()) {
-		int t = pq.top().first;
-		int n = pq.top().second;
-		pq.pop();
-		if (n == K)
-			return t - 1;
-		if (n * 2 <= 100000 && !visited[n * 2]) {
-			visited[n * 2] = 1;
-			pq.push(make_pair(t, n * 2));
-		}
-		if (n + 1 <= 100000 && !visited[n + 1]) {
-			visited[n + 1] = 1;
-			pq.push(make_pair(t + 1, n + 1));
-		}
-		if (n - 1 >= 0 && !visited[n - 1]) {
-			visited[n - 1] = 1;
-			pq.push(make_pair(t + 1, n - 1));
-		}
-	}
-}
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
 
 int main() {
 	FASTIO;
+	int N, K;
+	vector<int> visited(100001, 0);
 	cin >> N >> K;
-	cout << BFS();
+	queue<int> q;
+	visited[N] = 1;
+	q.push(N);
+	
+	while (!q.empty()) {
+		int node = q.front();
+		q.pop();
+		if (node == K) break;
+		int next = node * 2;
+		while (next != 0 && next <= 100000) {
+			if (!visited[next]) {
+				visited[next] = visited[node];
+				q.push(next);
+			}
+			next *= 2;
+		}
+		if (node >= 1 && !visited[node - 1]) {
+			visited[node - 1] = visited[node] + 1;
+			q.push(node - 1);
+		}
+		if (node < 100000 && !visited[node + 1]) {
+			visited[node + 1] = visited[node] + 1;
+			q.push(node + 1);
+		}
+	}
+	cout << visited[K] - 1;
 
 	return 0;
 }
