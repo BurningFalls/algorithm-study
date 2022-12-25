8632c989263bb53e60d4acb7a13cce5e59ae0159
BurningFalls <sherlocky31415@gmail.com>
2022-12-25 15:35:29 +0900

    [UPDATE] BOJ 02448

--- Baekjoon_All/(02000~02999)/(02448) 별 찍기 - 11.cpp
+++ Baekjoon_All/(02000~02999)/(02448) 별 찍기 - 11.cpp
@@ -1,41 +1,46 @@
-#include <iostream>
-#include <math.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool arr[3072][6143];
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
 
-void star(int n, int y, int x) {
+int N;
+vector<vector<char>> v(3073, vector<char>(6145, ' '));
+
+void DFS(int n, int y, int x) {
 	if (n == 3) {
-		arr[y][x] = 1;
-		arr[y + 1][x - 1] = 1;
-		arr[y + 1][x] = 0;
-		arr[y + 1][x + 1] = 1;
-		arr[y + 2][x - 2] = 1;
-		arr[y + 2][x - 1] = 1;
-		arr[y + 2][x] = 1;
-		arr[y + 2][x + 1] = 1;
-		arr[y + 2][x + 2] = 1;
+		v[y][x] = '*';
+		v[y + 1][x - 1] = '*';
+		v[y + 1][x + 1] = '*';
+		FOR(i, x - 2, x + 2) {
+			v[y + 2][i] = '*';
+		}
 		return;
 	}
-	star(n / 2, y, x);
-	star(n / 2, y + n / 2, x - n / 2);
-	star(n / 2, y + n / 2, x + n / 2);
+	DFS(n / 2, y, x);
+	DFS(n / 2, y + n / 2, x - n / 2);
+	DFS(n / 2, y + n / 2, x + n / 2);
 }
 
 int main() {
-	int N;
-	int step;
+	FASTIO;
 	cin >> N;
-	step = sqrt(N / 3);
-	star(N, 0, N - 1);
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < 2 * N - 1; j++) {
-			if (arr[i][j] == 1)
-				cout << '*';
-			else if (arr[i][j] == 0)
-				cout << " ";
+
+	DFS(N, 0, N - 1);
+
+	FOR(i, 0, N - 1) {
+		FOR(j, 0, N * 2 - 1) {
+			cout << v[i][j];
 		}
-		cout << endl;
+		cout << "\n";
 	}
 
 	return 0;
