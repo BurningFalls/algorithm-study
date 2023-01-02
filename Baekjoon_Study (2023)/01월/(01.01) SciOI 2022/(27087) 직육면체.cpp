75e0d02f716e048a90e6c33e2004903f3bcaaa25
BurningFalls <sherlocky31415@gmail.com>
2023-01-02 11:43:03 +0900

    Baekjoon Contest: SciOI 2022

---
+++ Baekjoon_Study (2023)/01월/(01.01) SciOI 2022/(27087) 직육면체.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int T;
+	cin >> T;
+	while (T--) {
+		int A, B, C, P;
+		cin >> A >> B >> C >> P;
+		int tmp = (A % P == 0) + (B % P == 0) + (C % P == 0);
+		cout << (tmp >= 2) << "\n";
+	}
+
+	return 0;
+}
