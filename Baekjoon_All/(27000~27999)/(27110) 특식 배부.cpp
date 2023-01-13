b3981b481b559f5adea61d03ef13166bd4c4de48
BurningFalls <sherlocky31415@gmail.com>
2023-01-13 16:36:32 +0900

    [CREATE] BOJ 27110

---
+++ Baekjoon_All/(27000~27999)/(27110) 특식 배부.cpp
@@ -0,0 +1,24 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int N;
+	int A, B, C;
+	cin >> N;
+	cin >> A >> B >> C;
+	cout << min(N, A) + min(N, B) + min(N, C);
+
+	return 0;
+}
