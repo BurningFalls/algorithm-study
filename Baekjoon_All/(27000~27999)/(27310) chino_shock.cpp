c11c057e8b0c77e893e6ba04ad868f7f33afa944
BurningFalls <sherlocky31415@gmail.com>
2023-02-09 12:24:03 +0900

    [CREATE] BOJ 27310

---
+++ Baekjoon_All/(27000~27999)/(27310) chino_shock.cpp
@@ -0,0 +1,27 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	string s;
+	cin >> s;
+	int a = 0, b = 0;
+	FOR(i, 0, LEN(s) - 1) {
+		a += (s[i] == ':');
+		b += (s[i] == '_');
+	}
+	cout << LEN(s) + a + b * 5;
+
+	return 0;
+}
