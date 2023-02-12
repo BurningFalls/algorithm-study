f5714409eaf4a5ca2b4048a6486f3ead387db717
BurningFalls <sherlocky31415@gmail.com>
2023-02-12 12:43:12 +0900

    [UPDATE] BOJ 25307

--- Baekjoon_All/(25000~25999)/(25307) 시루의 백화점 구경.cpp
+++ Baekjoon_All/(25000~25999)/(25307) 시루의 백화점 구경.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+﻿#include <bits/stdc++.h>
 using namespace std;
 
 #define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
@@ -11,58 +11,53 @@ using namespace std;
 #define ALL(v) v.begin(),v.end()
 #define INF (int)2e9
 #define LINF (ll)1e18
-#define MAX 2001
 
 int N, M, K;
-vector<vector<int>> Map(MAX, vector<int>(MAX));
-vector<vector<int>> visited(MAX, vector<int>(MAX, 0));
-int dy[4] = { -1, 1, 0, 0 };
-int dx[4] = { 0, 0, -1, 1 };
-queue<pii> q;
+vector<vector<int>> Map;
+queue<pii> q1, q2;
+vector<vector<int>> visited;
+vector<int> dy = { -1, 1, 0, 0 };
+vector<int> dx = { 0, 0, -1, 1 };
 
-void BFS_man() {
-	while (!q.empty()) {
-		int y = q.front().first;
-		int x = q.front().second;
-		q.pop();
-		if (visited[y][x] == K + 1) continue;
+void BFS1() {
+	while (!q1.empty()) {
+		int y = q1.front().first;
+		int x = q1.front().second;
+		q1.pop();
+		if (visited[y][x] - 1 == K) continue;
 		FOR(i, 0, 3) {
 			int ny = y + dy[i];
 			int nx = x + dx[i];
 			if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
 			if (visited[ny][nx]) continue;
-			if (Map[ny][nx] == 4) continue;
 			visited[ny][nx] = visited[y][x] + 1;
-			Map[ny][nx] = 1;
-			q.push({ ny, nx });
+			q1.push({ ny, nx });
+			if (Map[ny][nx] <= 2) {
+				Map[ny][nx] = 3;
+			}
 		}
 	}
 }
 
-int BFS(int sy, int sx) {
-	FOR(i, 0, N - 1) {
-		FOR(j, 0, M - 1) {
-			visited[i][j] = 0;
-		}
-	}
-	queue<pii> q;
+int BFS2(int sy, int sx) {
+	visited = vector<vector<int>>(N, vector<int>(M, 0));
 	visited[sy][sx] = 1;
-	q.push({ sy, sx });
-	while (!q.empty()) {
-		int y = q.front().first;
-		int x = q.front().second;
-		q.pop();
+	q2.push({ sy, sx });
+	while (!q2.empty()) {
+		int y = q2.front().first;
+		int x = q2.front().second;
+		q2.pop();
+		if (Map[y][x] == 2) {
+			return visited[y][x] - 1;
+		}
 		FOR(i, 0, 3) {
 			int ny = y + dy[i];
 			int nx = x + dx[i];
 			if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
 			if (visited[ny][nx]) continue;
-			if (Map[ny][nx] == 1) continue;
+			if (Map[ny][nx] != 0 && Map[ny][nx] != 2) continue;
 			visited[ny][nx] = visited[y][x] + 1;
-			if (Map[ny][nx] == 2) {
-				return visited[y][x];
-			}
-			q.push({ ny, nx });
+			q2.push({ ny, nx });
 		}
 	}
 	return -1;
@@ -70,27 +65,24 @@ int BFS(int sy, int sx) {
 
 int main() {
 	FASTIO;
+	int sy = 0, sx = 0;
 	cin >> N >> M >> K;
-	int py = 0, px = 0;
+	Map = vector<vector<int>>(N, vector<int>(M));
+	visited = vector<vector<int>>(N, vector<int>(M, 0));
 	FOR(i, 0, N - 1) {
 		FOR(j, 0, M - 1) {
 			cin >> Map[i][j];
-			if (Map[i][j] == 4) {
-				py = i, px = j;
-			}
-		}
-	}
-	FOR(i, 0, N - 1) {
-		FOR(j, 0, M - 1) {
 			if (Map[i][j] == 3) {
+				q1.push({ i, j });
 				visited[i][j] = 1;
-				Map[i][j] = 1;
-				q.push({ i, j });
+			}
+			else if (Map[i][j] == 4) {
+				sy = i, sx = j;
 			}
 		}
 	}
-	BFS_man();
-	cout << BFS(py, px);
+	BFS1();
+	cout << BFS2(sy, sx);
 
 	return 0;
 }
