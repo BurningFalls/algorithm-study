c369333bab359a4794b14cfa8678b5261c947aaf
BurningFalls <sherlocky31415@gmail.com>
2023-02-12 13:10:21 +0900

    [UPDATE] BOJ 04355

--- Baekjoon_All/(04000~04999)/(04355) 서로소.cpp
+++ Baekjoon_All/(04000~04999)/(04355) 서로소.cpp
@@ -6,15 +6,11 @@ using namespace std;
 #define ROF(i,a,b) for(int i=(a);i>=(b);i--)
 #define ll long long int
 #define pii pair<int, int>
-#define vi vector<int>
-#define vvi vector<vector<int>>
 #define PQ priority_queue
-#define LEN(v) int(v.size())
+#define LEN(v) (int)v.size()
 #define ALL(v) v.begin(),v.end()
-#define INF 2e9
-#define LINF 1e18
-#define P1(a) cout << a << "\n"
-#define P2(a, b) cout << a << " " << b << "\n";
+#define INF (int)2e9
+#define LINF (ll)1e18
 
 int main() {
 	FASTIO;
@@ -22,8 +18,13 @@ int main() {
 		int N;
 		cin >> N;
 		if (N == 0) break;
+		if (N == 1) {
+			cout << 0 << "\n";
+			continue;
+		}
 		int ans = N;
-		for (int i = 2; i * i <= N; i++) {
+		int n = N;
+		for (int i = 2; i * i <= n; i++) {
 			if (N % i == 0) {
 				ans -= ans / i;
 				while (N % i == 0) {
