3849bb4fe516940758996cd5e5ae97f1e2f3783b
BurningFalls <sherlocky31415@gmail.com>
2023-03-12 13:28:54 +0900

    [CREATE] BOJ 27648

---
+++ Baekjoon_All/(27000~27999)/(27648) 증가 배열 만들기.cpp
@@ -0,0 +1,34 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int N, M, K;
+	cin >> N >> M >> K;
+	vector<vector<int>> v(N, vector<int>(M, 0));
+	if (N + M - 1 > K) {
+		cout << "NO";
+	}
+	else {
+		cout << "YES\n";
+		FOR(i, 0, N - 1) {
+			FOR(j, 1, M) {
+				cout << i + j << " ";
+			}
+			cout << "\n";
+		}
+	}
+
+	return 0;
+}
