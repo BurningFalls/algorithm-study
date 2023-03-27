2a268af4ece0f2b99bbaabe4df2e6999c2b58282
BurningFalls <sherlocky31415@gmail.com>
2023-03-27 13:20:21 +0900

    Baekjoon Contest: 2023 UDPC (Solve: A, B, C, D, E)

---
+++ Baekjoon_Study (2023)/03월/(03.26) 2023 UDPC/(27918) 탁구 경기.cpp
@@ -0,0 +1,32 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+	FASTIO;
+	int N;
+	cin >> N;
+	vector<char> v(N);
+	FOR(i, 0, N - 1) {
+		cin >> v[i];
+	}
+	int d = 0, p = 0;
+	FOR(i, 0, N - 1) {
+		d += (v[i] == 'D');
+		p += (v[i] == 'P');
+		if (abs(d - p) >= 2) break;
+	}
+	cout << d << ":" << p;
+
+	return 0;
+}
