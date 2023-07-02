6d8261f202a8100c08a1e4e5d4e752809c39fe19
BurningFalls <sherlocky31415@gmail.com>
2023-07-02 15:14:21 +0900

    [CONTEST] UCPC 2023 예선

---
+++ Baekjoon_Study (2023)/07월/(07.01) UCPC 2023 예선/(28295) 체육은 코딩과목 입니다.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define ROF(i,a,b) for(int i=(a);i>=(b);i--)
+#define ll long long int
+#define pii pair<int, int>
+#define PQ priority_queue
+#define LEN(v) (int)v.size()
+#define ALL(v) v.begin(),v.end()
+#define INF (int)2e9
+#define LINF (ll)1e18
+
+int main() {
+    FASTIO;
+    int dir = 0;
+    vector<char> v = {'N', 'E', 'S', 'W'};
+    FOR(i, 1, 10) {
+        int t;
+        cin >> t;
+        if (t == 1) {
+            dir = (dir + 1) % 4;
+        }
+        else if (t == 2) {
+            dir = (dir + 2) % 4;
+        }
+        else if (t == 3) {
+            dir = (dir + 3) % 4;
+        }
+    }
+    cout << v[dir];
+    
+    return 0;
+}
+
+
+
+
+
